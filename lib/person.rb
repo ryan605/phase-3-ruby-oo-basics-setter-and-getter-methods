fd4b77e8699a13886a2e3cbc6a6c7086b5893aa5
ryan605 <langatryan26@gmail.com>
2023-02-13 15:08:34 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
 
+    def name=(name)
+        @name="Beyonce"
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = "Singer"
+    end
+
+    def job
+        @job
+    end
 end
